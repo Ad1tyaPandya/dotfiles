146a5ad196899749c278343aa7e040f917cf887e
Aditya <adi@adityas-air-4.lan>
2024-02-20 12:28:04 -0500

    add config for lsp, tree,pandoc,and telescope. Added zet command as exe file and added to scripts file to path

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# first run chmod u+x ./setup.sh
+# then ./setup.sh
+# to run this file
+
+# Second Brain
+export SECOND_BRAIN="/Users/adi/Library/Mobile Documents/iCloud~md~obsidian/Documents/Brain"
+ln -sf "$SECOND_BRAIN" ~/brain
+
+echo "complete setup"
